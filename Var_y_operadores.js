b0cb5299d3fb49df87a9200ba85376dd26d320f6
rosario <rosarioayyala@gmail.com>
2021-03-25 21:45:09 -0300

    Punto_a

---
+++ Var_y_operadores.js
@@ -0,0 +1,14 @@
+ var x=4;
+ var y=13;
+var suma= x +y ;
+console.log("Punto a) ",suma);
+
+var x1='Aprendiendo';
+var x2= ' JS';
+var palabra= x1 + x2;
+console.log("Punto b)",palabra);
+
+var y1=x1.length
+var y2=x2.length;
+var suma2= y1 + y2;
+console.log("Punto b)",suma2);
