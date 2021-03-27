be8fdd56df980d949b51e867784900e91ae9b07b
rosario <rosarioayyala@gmail.com>
2021-03-27 17:16:22 -0300

    for

---
+++ For.js
@@ -0,0 +1,27 @@
+ var x1=['acero','cobre','latón','aluminio','plata'];
+/*for(let index=0; index<x1.length;index++){
+    alert(x1[index]);
+}*/
+var z1=[];
+var z2=[];
+var metals=[];
+
+/*for(let i=0; i<x1.length;i++){
+    z1[i]= x1[i].charAt(0).toUpperCase();
+    z2[i]=x1[i].slice(1).toLowerCase();
+   metals[i]=z1[i]+z2[i]
+
+   alert(metals[i]);
+}*/
+
+/*for(let i=0;i<x1.length;i++){
+    sentence=x1.join(" ");
+   
+}
+alert(sentence)*/
+
+var contador=[];
+for (let i=0;i < 10 ;i++) {
+  contador[i]=i;
+}
+console.log(contador)
---
+++ If_Else.js
@@ -0,0 +1,26 @@
+/*x1=Math.random();
+console.log(x1);
+if (x1 < 0.5){
+    alert('Lower than 0,5')
+}
+    else{
+        alert('Greater than 0,5')
+    } 
+*/
+
+/*var age=1;
+if (age < 2){
+    alert('Bebé');
+} else if(age >=2 && age<=12) {
+    alert('Niño')
+} else if (age>12 &&  age<=19) {
+    alert('Adolescente')
+} else if (age>19 && age<=30){
+    alert('Joven')
+} else if (age>30 && age<=60) {
+    alert('Adulto')
+}else if (age>60 && age<=75){
+    alert('Adulto mayor')
+ } else {
+     alert('Anciano')
+}*/
