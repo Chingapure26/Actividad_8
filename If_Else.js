492aa0e3a5bdcd24bde26b7fe5f1d69a547622e7
rosario <rosarioayyala@gmail.com>
2021-03-28 13:36:49 -0300

    Ejercicio_4_If_Else

--- If_Else.js
+++ If_Else.js
@@ -1,4 +1,5 @@
-/*x1=Math.random();
+/*Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”*/ 
+x1=Math.random();
 console.log(x1);
 if (x1 < 0.5){
     alert('Lower than 0,5')
@@ -6,9 +7,10 @@ if (x1 < 0.5){
     else{
         alert('Greater than 0,5')
     } 
-*/
 
-/*var age=1;
+/*Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+*/ 
+var age=1;
 if (age < 2){
     alert('Bebé');
 } else if(age >=2 && age<=12) {
@@ -23,4 +25,4 @@ if (age < 2){
     alert('Adulto mayor')
  } else {
      alert('Anciano')
-}*/
+}
