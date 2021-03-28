26235695cfbff0fa57ad4b57a2af737b6909da86
rosario <rosarioayyala@gmail.com>
2021-03-28 13:25:38 -0300

    Ejer_1_Var_y_Operadores

--- Var_y_operadores.js
+++ Var_y_operadores.js
@@ -1,13 +1,16 @@
- var x=4;
+/*Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+*/
+var x=4;
  var y=13;
 var suma= x +y ;
 console.log("Punto 1.a) ",suma);
-
+/*Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/ 
 var x1='Aprendiendo';
 var x2= ' JS';
 var palabra= x1 + x2;
 console.log("Punto 1.b)",palabra);
-
+/*Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
+ */
 var y1=x1.length
 var y2=x2.length;
 var suma2= y1 + y2;
